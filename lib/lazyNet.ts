70df9462bf7dd4323a512c1d792d9bbbed60ccd1
DevzCat <HajimeDevLife@gmail.com>
2022-02-10 15:01:41 +0700

    change source code.

--- lib/lazyNet.ts
+++ lib/lazyNet.ts
@@ -1,35 +1,14 @@
-import { isNumberObject } from 'util/types'
 import thisNet from './findNet'
-import { isNumber } from './utils'
-
-const errorLog = `Error: at isLazy(port) function.\n
-  Log: 'port' is type number.`
+import lazyOutline from '@koihii/lazy-outline'
 
 function showOutline(port: number) {
   const standardLog = [
-    '  |  Your server is running on:',
-    `  |    Local:            http://localhost:${port}`,
-    `  |    On Your Network:  http://${thisNet['eth0']}:${port}`,
+    'Your server is running on:',
+    `    Local:            http://localhost:${port}`,
+    `    On Your Network:  http://${thisNet['eth0']}:${port}`,
   ]
 
-  const length = standardLog[2].length + 3
-
-  for (let i = 0; i < 7; i++) {
-    if (i === 0 || i === 6) {
-      const line = `  ${'-'.repeat(length - 2)}`
-      console.log(`${i === 0 ? '\n' : ''}${line}${i === 6 ? '\n' : ''}`)
-    } else if (i === 1 || i === 5) {
-      console.log(`  |${' '.repeat(length - 4)}|`)
-    } else if (i === 2) {
-      const restLength = length - standardLog[0].length - 1
-      console.log(`${standardLog[0]}${' '.repeat(restLength)}|`)
-    } else if (i === 3) {
-      const restLength = length - standardLog[1].length - 1
-      console.log(`${standardLog[1]}${' '.repeat(restLength)}|`)
-    } else {
-      console.log(`${standardLog[2]}  |`)
-    }
-  }
+  lazyOutline(standardLog)
 }
 
 function noShowOutline(port: number) {
@@ -46,8 +25,6 @@ function noShowOutline(port: number) {
 }
 
 function lazyNet(port: number, outline = false) {
-  if (!isNumber(port)) throw errorLog
-
   if (outline) return showOutline(port)
   return noShowOutline(port)
 }
